d38cd51cd82de5c8d59adad215ec98047ed19264
本間敬太 <keitakunv2@gmail.com>
2025-03-24 21:22:04 +0900

    ✨ 読み込み中画面を追加

---
+++ src/app/info/[slug]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Define the Loading UI here
+  return (
+    <div className="border border-black rounded-md m-3 p-3">
+      <h2 className="text-xl flex items-center justify-center">Loading...</h2>
+    </div>
+  );
+}
